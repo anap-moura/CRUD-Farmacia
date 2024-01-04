b7a134701092e25a539fb79fa7a4a44b648877b9
Ana Paula Moura <ana.mcruz@hotmail.com>
2024-01-04 14:34:43 -0300

    application.properties e pom.xml configurados

---
+++ src/main/java/com/generation/farmaciaspring/FarmaciaspringApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/farmaciaspring/FarmaciaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmaciaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
